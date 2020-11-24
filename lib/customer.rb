0e4788b3fe06d59338b381f58f96eaa0561bfd16
Charlie Knight <williamchknight@gmail.com>
2020-11-24 12:32:05 -0800

    done

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,36 @@
+
 class Customer
   
+    attr_reader :name
+    attr_accessor :age 
+
+    @@all = []
+    def initialize(name,age)
+        @name = name
+        @age = age
+        @@all << self 
+    end 
+    
+    def self.all 
+        @@all
+    end 
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end 
+
+    def meals
+        Meal.all.select {|m| m.customer == self}
+    end 
+
+    def waiters
+        waiters = []
+        Meal.all.each do |m|
+            if m.customer == self
+                waiters << m.waiter
+            end
+        end 
+    waiters 
+    end 
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
+    attr_reader :name
+    attr_accessor :experience
 
+    @@all = []
+    def initialize(name, experience)
+        @name = name
+        @experience = experience
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end 
+
+    def meals
+        Meal.all.select {|m| m.waiter == self}
+    end 
+
+    def best_tipper
+        meals.max_by {|m| m.tip}.customer 
+    end 
 end
